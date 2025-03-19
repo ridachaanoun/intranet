f9e7793321d06014c49b22e0e48df56b7772632d
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-03-19 11:55:19 +0000

    Update the classrooms Table Migration

--- database/migrations/2025_03_18_145727_create_classes_table.php
+++ database/migrations/2025_03_18_145727_create_classes_table.php
@@ -11,14 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('classes', function (Blueprint $table) {
+        Schema::create('classrooms', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
             $table->string('name');
-            $table->unsignedBigInteger('promotion_id');
-            $table->unsignedBigInteger('teacher_id');
+            $table->string('level');
+            $table->integer('level_value');
+            $table->string('campus');
+            $table->integer('campus_value');
+            $table->integer('learners');
+            $table->unsignedBigInteger('promotion_id')->nullable();
+            $table->string('cover_image')->nullable();
+            $table->unsignedBigInteger('teacher_id')->nullable();
+            $table->unsignedBigInteger('delegate_id')->nullable();
             $table->timestamps();
+
+            // Foreign key constraints
             $table->foreign('promotion_id')->references('id')->on('promotions')->onDelete('set null');
             $table->foreign('teacher_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('delegate_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
