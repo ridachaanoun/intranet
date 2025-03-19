e65d77ab0dcde0fff64a8685b4c90c6f7860ec77
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-03-19 09:50:27 +0000

    create classe student table

---
+++ database/migrations/2025_03_18_150223_create_class_student_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('class_student', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('class_id');
+            $table->unsignedBigInteger('student_id');
+            $table->timestamps();
+
+            $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
+            $table->foreign('student_id')->references('id')->on('users')->onDelete('cascade');
+
+            // Ensure unique pairing
+            $table->unique(['class_id', 'student_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('class_student');
+    }
+};
