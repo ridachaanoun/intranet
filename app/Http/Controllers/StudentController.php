2b1c927b2a571c480b3787591166feacf107f2c0
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-04-18 22:14:43 +0100

    Refactor getAllStudents method to return raw student data instead of formatted output

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,21 +10,8 @@ class StudentController extends Controller
         // Fetch all users with the role of 'student'
         $students = User::where('role', 'student')->orderBy('name', 'asc')->get();
 
-        $formattedStudents = $students->map(function ($student) {
-            return [
-                'id' => $student->id,
-                'name' => $student->name,
-                'email' => $student->email,
-                'image_url' => asset('storage/' . $student->image),
-                'campus' => $student->campus,
-                'level' => $student->level,
-                'created_at' => $student->created_at,
-                'updated_at' => $student->updated_at,
-            ];
-        });
-
         return response()->json([
-            'students' => $formattedStudents,
+            'students' => $students,
         ], 200);
     }
 }
